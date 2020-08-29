dbf0a212ba128d50e317df2b3c663d6d5f52f478
Josh <joshhsoj1902@gmail.com>
2020-08-29 13:51:04 -0400

    fix jq output and bash ifs

--- healthy.sh
+++ healthy.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 repo_path="https://github.com"
 api_query=""
 
 # https://developer.github.com/v3/actions/self-hosted-runners/
-if [ "$ORG_RUNNER" = "false" ]; then
+if [[ $ORG_RUNNER == "false" ]]; then
     api_path="https://api.github.com/repos"
     api_query=`echo $REPO_URL | sed  "s|$repo_path|$api_path|g"`"/actions/runners"
 else
@@ -12,9 +12,9 @@ else
     api_query=$api_path"/"$ORG_NAME"/actions/runners"
 fi
 
-status=`curl -s -H "Authorization: token ${ACCESS_TOKEN}" ${api_query} | jq '.runners[] | select(.name=="'$HOSTNAME'") | .status'`
+status=`curl -s -H "Authorization: token ${ACCESS_TOKEN}" ${api_query} | jq -r '.runners[] | select(.name=="'$HOSTNAME'") | .status'`
 
-if [ "$status" = "online" ]; then
+if [[ $status == "online" ]]; then
     exit 0
 fi
 
